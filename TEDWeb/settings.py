74731402bc3137a5ddae8a9d1789f40d34649139
Yash Moda <yashmoda07@gmail.com>
2020-01-25 15:34:25 +0530

    Static and media root added.

--- TEDWeb/settings.py
+++ TEDWeb/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/js/'
+STATIC_ROOT = os.path.join(BASE_DIR, "js")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
