8f46e29f82f57d183bea868dd045cf4489c3fd42
Yash Moda <yashmoda07@gmail.com>
2020-01-25 15:20:47 +0530

    Static and media root added.

--- TEDWeb/settings.py
+++ TEDWeb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TEDWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
