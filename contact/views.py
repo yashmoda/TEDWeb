fa35bdb2ca51e3fc9e017d2f6e65fcb9c8c82ff8
Yash Moda <yashmoda07@gmail.com>
2020-01-28 19:22:35 +0530

    Urls and testing of contact us completed.

--- contact/views.py
+++ contact/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-#name
-#phone_no
-#query
+from contact.models import Queries
+
 
 @csrf_exempt
 def contact_us(request):
+    response_json = {}
     try:
-        response_json= {}
         if request.method=='POST':
-            name= request.POST.get('name')
-            phone_no= request.POST.get('phone_no')
-            query= request.POST.get('query')
-
-
-            if Queries.objects.filter(phone_no= phone_no).count()>0:
+            name = request.POST.get('name')
+            phone_no = request.POST.get('phone_no')
+            query = request.POST.get('query')
+            if Queries.objects.filter(phone = phone_no).count()>0:
                 response_json['success']= True
                 response_json['message']= "You have already applied. We will contact you shortly."
                 return JsonResponse(response_json)
 
             else:
-                query1= Queries.objects.create(name=name, phone_no= phone_no, query=query)
-
+                query1= Queries.objects.create(name=name, phone = phone_no, query=query)
                 response_json['success']= True
                 response_json['message']= "You will recieve an email shortly."
                 return JsonResponse(response_json)
