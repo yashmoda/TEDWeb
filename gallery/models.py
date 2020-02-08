ec105aadd4b269fb80979fd5d1e26ea525667456
Hardik <kunwarhardik1999@gmail.com>
2020-02-08 19:06:00 +0530

    added gallery model,urls,views

--- gallery/models.py
+++ gallery/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+    description = models.CharField(max_length = 256)
+    image = models.ImageField(null = True, blank = True, upload_to = '')
+    year = models.IntegerField()
+    is_deleted = models.BooleanField(default = False)
+    modified = models.DateTimeField(auto_now = True , auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+def __str__(self):
+    return self.name
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+from gallery.models import Gallery
 
 # Create your views here.
+
+@csrf_exempt
+def gallery_feed(request):
+    response_Json = {'image_feed': []}
+    if request.method == 'GET':
+        try:
+            ted_gallery_feed = Gallery.objects.all().order_by('-id')
+            for event_image in ted_gallery_feed:
+                temp_json = {'description': image.description, 'year': event_image.year,
+                               'image': request.scheme + '://' + request.get_host() +
+                                '/media/' + str(event_image.image)}
+          
+                response_Json['image_feed'].append(temp_json)
+            response_Json['success'] = True
+            response_Json['message'] = 'Images of previous TEDx event has been succesfully shown'
+            return JsonResponse(response_Json)
+
+
+
+
+    except Exception as e:
+        print str(e)
+        response_Json['Failed'] = False
+        response_Json['message'] = 'An error has occured. Please try again later'
+        return JsonResponse(response_Json)
