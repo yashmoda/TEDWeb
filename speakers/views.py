4abace90b038f4d44bd679e688134a0800e87c3c
Hardik <kunwarhardik1999@gmail.com>
2020-02-26 21:19:52 +0530

    update 2.0

--- speakers/views.py
+++ speakers/views.py
@@ -107,7 +107,8 @@ def current_speakers(request):
                              'domain': current_speaker.domain,
                              'image': request.scheme + '://' + request.get_host() +
                                       '/media/' + str(current_speaker.image),
-                             'id': current_speaker.id}
+                             'id': current_speaker.id,
+                             'description': current_speaker.description}
                 response_json['speaker_details'].append(temp_json)
             response_json['success'] = True
             response_json['message'] = "The list of current speakers has been successfully shown."
