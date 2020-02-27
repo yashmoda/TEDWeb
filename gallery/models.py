c9c4e69ca17ba4d0a291bf130fb9a8894a7a4cfc
Yash Moda <yashmoda07@gmail.com>
2020-02-27 21:24:14 +0530

    migrations added.

--- gallery/models.py
+++ gallery/models.py
@@ -16,5 +16,5 @@ class Gallery(models.Model):
     modified = models.DateTimeField(auto_now = True , auto_now_add=False)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.year
